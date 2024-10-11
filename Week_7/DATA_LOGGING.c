f28841158a775d0365525a5ec9794dd94e99c38e
blue-puma <113889625+blue-puma@users.noreply.github.com>
2024-10-11 14:04:11 +0900

    Update DATA_LOGGING.c

--- Week_7/DATA_LOGGING.c
+++ Week_7/DATA_LOGGING.c
@@ -113,6 +113,7 @@ int main(void)
 
     errorPosition = referencePosition - redGearPosition;
     checkTimeBefore = millis();
+    startTime = millis();
     while(1)
     {
         checkTime = millis();
